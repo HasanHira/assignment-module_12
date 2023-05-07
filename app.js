e5349350ea43e7800b43240db031380687e92983
HasanHira <qubitgraphix@gmail.com>
2023-05-08 00:40:09 +0600

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+/*
+Using JavaScript, write a function that calculates the Body Mass Index (BMI) of a person when the "calculateBtn" is clicked.
+
+The BMI formula is: BMI = weight (kg) / (height (m) * height (m)).
+
+*/
