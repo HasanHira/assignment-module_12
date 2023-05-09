6a842052541b02e9bd85f9f966f8ddf25963a179
HasanHira <qubitgraphix@gmail.com>
2023-05-09 18:35:53 +0600

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,41 @@
-/*
-Using JavaScript, write a function that calculates the Body Mass Index (BMI) of a person when the "calculateBtn" is clicked.
 
-The BMI formula is: BMI = weight (kg) / (height (m) * height (m)).
+// BMI calculate
+let weightInput = document.getElementById('weightInput');
+let heightInput = document.getElementById('heightInput');
+let calculateBtn = document.getElementById('calculateBtn');
+let result = document.getElementById('bmiResult');
 
-*/
+function bmiCalculate(e) {
+  e.preventDefault();
+  let weight = parseFloat(weightInput.value);
+  let height = parseFloat(heightInput.value) / 100;
+
+  if (isNaN(weight) || isNaN(height)) {
+    result.innerHTML = "Inputs are invalid or Empty";
+  } else {
+    let bmi = weight / (height * height);
+    let bmiScore = bmi.toFixed(2);
+    if (bmiScore<=18.5){
+      var mass = "Under Weight";
+    } else if(bmiScore>18.5 && bmiScore<=24.9){
+      var mass = "Normal Weight";
+    } else if(bmiScore>=25 && bmiScore<=29.9){
+      var mass = "Over Weight";
+    } else if(bmiScore>=30 && bmiScore<=39.9){
+      var mass = "Obese";
+    } else if(bmiScore>=40){
+      var mass = "Morbidly Obese";
+    }
+    result.innerHTML = "Your BMI is: <span>" + bmiScore + "</span>" + "<span>/m<sup>2</sup></span>" + "<h3>"+ mass +"</h3>";
+    // weightInput.value = '';
+    // heightInput.value = '';
+  }
+}
+calculateBtn.addEventListener('click', bmiCalculate);
+
+
+// Underweight = 18.5
+// Normal = 18.5 to 24.9
+// Overweight = 25 to 29.9
+// Obese = 30 to 39.9
+// Morbidly Obese = Over 40
